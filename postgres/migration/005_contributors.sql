c6d5fac2a86722d32e35dc3365af999759f6bd76
saiddis <saiddisruptive@gmail.com>
2024-11-25 18:22:19 +0500

    bug fixes

--- postgres/migration/005_contributors.sql
+++ postgres/migration/005_contributors.sql
@@ -1,7 +1,7 @@
 CREATE TABLE contributors (
 	id SERIAL PRIMARY KEY,
-	repo_id INT NOT NULL REFERENCES repos(id),
-	user_id INT NOT NULL REFERENCES users(id),
+	repo_id INT NOT NULL REFERENCES repos(id) ON DELETE CASCADE,
+	user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	UNIQUE(repo_id, user_id)
