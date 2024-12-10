3ac997e54a2e19130d8b5e1211522052bb7e8dd0
saiddis <saiddisruptive@gmail.com>
2024-12-10 18:46:54 +0500

    feat: add application constants

---
+++ todev.go
@@ -0,0 +1,15 @@
+package todev
+
+import "context"
+
+// Build version and commint SHA
+var (
+	Version string
+	Commit  string
+)
+
+// ReportError notifies an external service of errors. No-op by default.
+var ReportError = func(ctx context.Context, err error, args ...interface{}) {}
+
+// ReportPanic notifies an external service of panics. No-op by default.
+var ReportPanic = func(err interface{}) {}
