f457509b83406e3216cf5af4263c8457c375ced7
saiddis <saiddisruptive@gmail.com>
2024-12-11 18:36:19 +0500

    test: contributor handlers

---
+++ http/contributor_test.go
@@ -0,0 +1,27 @@
+package http_test
+
+// import (
+// 	"context"
+// 	"testing"
+// 	"time"
+//
+// 	"github.com/saiddis/todev"
+// )
+//
+// func TestContributorNew(t *testing.T) {
+// 	s := MustOpenServer(t)
+// 	defer MustCloseServer(t, s)
+//
+// 	user0 := &todev.User{ID: 1, Name: "user1", APIKey: "apiKey"}
+// 	ctx0 := todev.NewContextWithUser(context.Background(), user0)
+//
+// 	repo := &todev.Repo{
+// 		ID:         1,
+// 		UserID:     1,
+// 		Name:       "repo1",
+// 		InviteCode: "inviteCode",
+// 		CreatedAt:  time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
+// 		UpdatedAt:  time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
+// 	}
+//
+// }
