71501abdc5efa423e62524e547b930823381c77d
saiddis <saiddisruptive@gmail.com>
2024-12-13 18:21:42 +0500

    refactor: to implement new event service interface

--- mock/event.go
+++ mock/event.go
@@ -9,27 +9,26 @@ import (
 var _ todev.EventService = (*EventService)(nil)
 
 type EventService struct {
-	PublishEventFn    func(id int, event todev.Event) error
+	PublishEventFn    func(id int, event todev.Event)
 	SubscribeFn       func(ctx context.Context) (todev.Subscription, error)
 	GetSubscribtionFn func(id int) (todev.Subscription, bool)
 }
 
-func (s *EventService) PublishEvent(id int, event todev.Event) error {
-	return s.PublishEventFn(id, event)
+func (s *EventService) PublishEvent(id int, event todev.Event) {
+	s.PublishEventFn(id, event)
 }
 
 func (s *EventService) Subscribe(ctx context.Context) (todev.Subscription, error) {
 	return s.SubscribeFn(ctx)
 }
 
-func (s *EventService) GetSubscribtion(id int) (todev.Subscription, bool) {
+func (s *EventService) GetSubscription(id int) (todev.Subscription, bool) {
 	return s.GetSubscribtionFn(id)
 }
 
 type Subscription struct {
 	CloseFn func()
 	CFn     func() <-chan todev.Event
-	DoneFn  func() chan struct{}
 }
 
 func (s Subscription) Close() {
@@ -38,8 +37,4 @@ func (s Subscription) Close() {
 
 func (s Subscription) C() <-chan todev.Event {
 	return s.CFn()
-}
-
-func (s Subscription) Done() chan struct{} {
-	return s.DoneFn()
 }
