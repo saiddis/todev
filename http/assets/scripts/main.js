83bd94356054f9bb177f3dbc491584f898d27a37
saiddis <saiddisruptive@gmail.com>
2025-01-01 19:16:10 +0500

    feat: position footer

--- http/assets/scripts/main.js
+++ http/assets/scripts/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector("nav")
+const footer = document.querySelector("footer")
 const main = document.querySelector("main")
 main.style.top = nav.clientHeight + "px"
+footer.style.top = nav.clientHeight + main.clientHeight + "px"
+
 document.body.style.height = document.documentElement.clientHeight + nav.clientHeight + "px"
 
 document.addEventListener("DOMContentLoaded", () => {
