50a410ae65c5086f42831b1b79aaed59e385a0af
saiddis <saiddisruptive@gmail.com>
2025-01-12 18:57:58 +0500

    refactor: rename main.js to base.js

---
+++ http/assets/scripts/base.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector("nav")
+const footer = document.querySelector("footer")
+const main = document.querySelector("main")
+main.style.top = nav.clientHeight + "px"
+footer.style.top = nav.clientHeight + main.clientHeight + "px"
+
+document.body.style.height = document.documentElement.clientHeight + nav.clientHeight + "px"
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.body.addEventListener("htmx:beforeSwap", function(event) {
+		if (event.detail.xhr.status === 422) {
+			event.detail.shouldSwap = true;
+			event.detail.isError = false;
+		}
+	})
+})
+
+const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
+	const [name, value] = cookie.split("=")
+	acc[name] = decodeURIComponent(value);
+	return acc;
+}, {})
+
+const avatarURL = cookies["avatar"]
+
+if (avatarURL) {
+	const avatarImg = document.querySelector(".avatar")
+	avatarImg.src = avatarURL
+
+}
