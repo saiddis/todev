aa58032727eb801f672fda980393f96610897235
saiddis <saiddisruptive@gmail.com>
2025-01-13 23:03:23 +0500

    refactor: delete unused code

--- todev.go
+++ todev.go
@@ -3,9 +3,6 @@ package todev
 import (
 	"context"
 	"log"
-	"sync"
-
-	"github.com/joho/godotenv"
 )
 
 // Build version and commint SHA
@@ -20,26 +17,4 @@ var ReportError = func(ctx context.Context, err error, args ...interface{}) {}
 // ReportPanic notifies an external service of panics. No-op by default.
 var ReportPanic = func(err interface{}) {
 	log.Printf("panic: %v", err)
-}
-
-var (
-	once    sync.Once
-	envData map[string]string
-)
-
-const DefaultEnvFilePath = "~/todev/.env"
-
-// GetEnv returns a function for returning config values by key from the given
-// config path.
-func GetFromEnv(path, key string) (string, bool) {
-	var err error
-	once.Do(func() {
-		envData, err = godotenv.Read(path)
-		if err != nil {
-			log.Fatalf("error reading env file: %v", err)
-		}
-	})
-
-	val, ok := envData[key]
-	return val, ok
 }
