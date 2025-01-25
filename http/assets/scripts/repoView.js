e22bae5dd040beed1277065c07b3bc7cb8ea711d
saiddis <saiddisruptive@gmail.com>
2025-01-25 20:58:31 +0500

    refactor: separate task class and page for viewing repo & add events for task completion

--- http/assets/scripts/repoView.js
+++ http/assets/scripts/repoView.js
@@ -2,169 +2,62 @@ const repoInfo = document.getElementById('repo-info')
 const repoID = repoInfo.dataset.repoId
 const contributorID = repoInfo.dataset.contributorId
 const isAdmin = repoInfo.dataset.isAdmin
-
-class Task {
-	constructor(wrapper, description, id, isCompleted) {
-		this.id = id
-
-		this.parent = this.getParentElement()
-		this.remove = this.getRemoveElement()
-		this.checkBox = this.getCheckBoxElement()
-		this.checkBoxIcon = this.getCheckBoxIconElement()
-		this.description = this.getDescriptionElement(description)
-		this.checkBox.checked = isCompleted
-
-		this.wrapper = this.getWrapper(wrapper)
-
-		this.checkBox.onchange = (event) => this.onCheckBox(event)
-		this.remove.onclick = (event) => this.onRemove(event)
-	}
-
-	getParentElement() {
-		let label = document.createElement('label')
-		label.classList.add('task', 'checkbox', 'flex', 'center-h', 'gap')
-		label.setAttribute('task-id', this.id)
-		return label
-	}
-
-	getCheckBoxElement() {
-		let input = document.createElement('input')
-		input.type = 'checkbox'
-		return input
-	}
-
-	getCheckBoxIconElement() {
-		let span = document.createElement('span')
-		span.className = 'checkbox__icon'
-		return span
-	}
-
-	getRemoveElement() {
-		let cross = document.createElement('div')
-		cross.className = 'cross'
-		return cross
-	}
-
-	getDescriptionElement(value) {
-		let input = document.createElement('input')
-		input.className = 'description'
-		input.type = 'text'
-		input.value = value
-		return input
-	}
-
-	getWrapper(wrapper) {
-		this.parent.append(this.remove, this.checkBox, this.checkBoxIcon, this.description)
-		wrapper.append(this.parent)
-		return wrapper
-	}
-
-	async onRemove(event) {
-		try {
-			const resp = fetch('/tasks/' + this.id, {
-				method: 'DELETE',
-				headers: {
-					'Content-type': 'application/json',
-					'Accept': 'application/json',
-				}
-			})
-			console.log(resp)
-		} catch (err) {
-			console.error(err)
-			return false
-		}
-		this.wrapper.remove()
-		if (titleCompleted == completedTasksList.lastElementChild) {
-			completedTasksList.hidden = true
-		}
-	}
-
-	onCheckBox(event) {
-		if (!this.description.value.trim()) { // if its input is empty
-			this.description.focus()
-			return false; // prevent from adding the task to the .completed-task-list
-		} else if (event.target == this.remove) {
-			event.preventDefault()
-			return false
-		}
-
-		if (this.wrapper.parentElement == tasksList) {
-			this.description.style.opacity = '0.4'
-			if (completedTasksList.hidden) {
-				completedTasksList.hidden = false
-			}
-
-			completedTasksList.append(this.wrapper);
-
-		} else {
-			this.description.style.opacity = '1'
-			tasksList.prepend(this.wrapper)
-
-			if (titleCompleted == completedTasksList.lastElementChild) {
-				completedTasksList.hidden = true
-			}
-		}
-	}
-}
-
-document.addEventListener('add-task', function(event) {
-	const li = document.createElement('li')
-	li.classList.add('flex', 'center-h')
-
-	if (isAdmin) {
-		new Task(li, event.detail.description, event.detail.id, event.detail.isCompleted)
-	} else {
-		new UserTask(li, event.detail.description, event.detail.id, event.detail.isCompleted)
-	}
-
-	let target = event.target
-	if (target.hidden) {
-		target.hidden = false
-	}
-	event.target.append(li)
-})
-
-class UserTask extends Task {
-	getDescriptionElement(value) {
-		let p = document.createElement('p')
-		p.innerHTML = value
-		return p
-	}
-
-	getCheckBoxElement() {
-		let input = document.createElement('input')
-		input.type = 'checkbox'
-		input.onclick = (event) => {
-			event.preventDefault()
-		}
-		return input
-	}
-
-	onCheckBox(event) {
-		event.preventDefault()
-		return false
-	}
-
-	onRemove(event) {
-		event.preventDefault()
-		return false
-	}
-
-	getWrapper(wrapper) {
-		this.parent.append(this.checkBox, this.checkBoxIcon, this.description)
-		wrapper.append(this.parent)
-		return wrapper
-	}
-}
-
 const tasksPane = document.getElementById('tasks-pane')
 const tasksList = document.getElementById('tasks-list')
 const completedTasksList = document.getElementById('completed-tasks-list')
 const titleCompleted = document.getElementById('title-completed')
 const addTaskButton = document.getElementById('add-task-button')
+const copyContent = async (text) => {
+	try {
+		await navigator.clipboard.writeText(text);
+		console.log('Content copied to clipboard');
+	} catch (err) {
+		console.error('Failed to copy: ', err);
+	}
+}
 
-addTaskButton.onclick = () => {
-	addTask()
+tasksPane.addEventListener('escape-task', function(event) {
+	if (event.target.parentElement == tasksList) {
+		if (!event.detail.remove) {
+			completedTasksList.append(event.target)
+		} else {
+			event.target.remove()
+		}
+	} else if (event.target.parentElement == completedTasksList) {
+		if (!event.detail.remove) {
+			tasksList.append(event.target)
+		} else {
+			event.target.remove()
+		}
+	}
+
+	if (completedTasksList.childElementCount < 3) {
+		completedTasksList.hidden = true
+	} else {
+		completedTasksList.hidden = false
+	}
+})
+
+tasksPane.addEventListener('add-task', function(event) {
+	let li = document.createElement('li')
+	li.classList.add('flex', 'center-h')
+
+	if (isAdmin == 'true') {
+		new Task(li, event.detail.description, event.detail.id, event.detail.isCompleted)
+	} else {
+		new UserTask(li, event.detail.description, event.detail.id, event.detail.isCompleted)
+	}
+
+	if (event.target.hidden) {
+		event.target.hidden = false
+	}
+	event.target.append(li)
+})
+
+if (isAdmin == 'true') {
+	addTaskButton.onclick = () => {
+		addTask()
+	}
 }
 
 function addTask() {
@@ -180,74 +73,82 @@ function addTask() {
 			li.remove()
 			return
 		}
-
-		let description = input.value
-		let id = 0
-		let intRepoID = parseInt(repoID)
-		if (!intRepoID) {
-			return false
-		}
-
-		try {
-			const resp = await fetch('/tasks', {
-				method: 'POST',
-				headers: {
-					'Content-type': 'application/json',
-					'Accept': 'application/json',
-				},
-				body: JSON.stringify({
-					description: description,
-					repoID: intRepoID,
-				}),
-			})
-			const task = await resp.json()
-			id = task.id
-		} catch (err) {
-			console.error(err)
-			return
-		}
-
-		if (id) {
+		let task = await createTask(input.value)
+		if (task) {
 			input.remove()
-			new Task(li, description, id, false)
+			new Task(li, task.description, task.id, false)
 		}
 	}
-
-
 }
 
-const copyContent = async (text) => {
+async function createTask(description) {
 	try {
-		await navigator.clipboard.writeText(text);
-		console.log('Content copied to clipboard');
-	} catch (err) {
-		console.error('Failed to copy: ', err);
-	}
-};
 
+		const resp = await fetch('/tasks', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+				'Accept': 'application/json',
+			},
+			body: JSON.stringify({
+				description: description,
+				repoID: parseInt(repoID),
+			}),
+		})
+
+		if (resp.ok) {
+			const task = await resp.json()
+			return task
+		}
+		return null
+	} catch (err) {
+		console.error(err)
+		return null
+	}
+}
 
 function connect() {
 	const socket = new ReconnectingWebSocket((location.protocol == 'https:' ? 'wss:' : 'ws:') + '//' + location.host + '/events');
-	socket.addEventListenter('message', function(event) {
+	socket.onmessage = function(event) {
 		const e = JSON.parse(event.data)
-
 		switch (e.type) {
 			case 'task:added':
-			//const li = document.createElement('li')
-			//li.classList.add('flex', 'center-h')
+				const li = document.createElement('li')
+				li.classList.add('flex', 'center-h')
 
-			//if (isAdmin) {
-			//	new Task(li, e.payload.task.description, false)
-			//} else {
-			//	new UserTask(li, e.payload.task.description, false)
-			//}
-			case 'task:deleted':
-				taks = document.querySelector(`.task[task-id=${e.payload.id}]`)
-				if (task) {
-					task.remove()
+				if (isAdmin == 'true') {
+					new Task(li, e.payload.task.description, e.payload.task.id, false)
+				} else {
+					new UserTask(li, e.payload.task.description, e.payload.task.id, false)
 				}
+
+				tasksList.append(li)
+				break
+			case 'task:deleted':
+				let task = document.querySelector(`.task[data-task-id="${e.payload.id}"]`)
+				if (task != null) {
+					task.dispatchEvent(new CustomEvent('escape-task', {
+						bubbles: true,
+						detail: {
+							remove: true,
+						}
+					}))
+				} else {
+					console.log("couldn't find task by id: " + e.payload.id)
+				}
+				break
+			case 'task:completion_toggled':
+				let input = document.querySelector(`.task[data-task-id="${e.payload.id}"] input[type="checkbox"]`)
+				if (input != null) {
+					input.dispatchEvent(new Event('change', {
+						bubbles: false,
+					}))
+				} else {
+					console.log("couldn't find task by id: " + e.payload.id)
+				}
+				break
 		}
-	})
+	}
 }
 
 connect()
---
+++ http/assets/scripts/task.js
@@ -0,0 +1,199 @@
+class Task {
+	constructor(wrapper, description, id, isCompleted) {
+		this.id = id
+
+		this.task = this.getTaskElement()
+		this.remove = this.getRemoveElement()
+		this.checkBox = this.getCheckBoxElement()
+		this.checkBoxIcon = this.getCheckBoxIconElement()
+		this.description = this.getDescriptionElement(description)
+		this.checkBox.checked = isCompleted
+
+		this.wrapper = this.getWrapper(wrapper)
+
+		this.checkBox.onchange = (event) => this.onCheckBox(event)
+		this.remove.onclick = (event) => this.onRemove(event)
+	}
+
+	getTaskElement() {
+		let label = document.createElement('label')
+		label.classList.add('checkbox', 'flex', 'center-h', 'gap')
+		return label
+	}
+
+	getCheckBoxElement() {
+		let input = document.createElement('input')
+		input.type = 'checkbox'
+		return input
+	}
+
+	getCheckBoxIconElement() {
+		let span = document.createElement('span')
+		span.className = 'checkbox__icon'
+		return span
+	}
+
+	getRemoveElement() {
+		let cross = document.createElement('div')
+		cross.className = 'cross'
+		return cross
+	}
+
+	getDescriptionElement(value) {
+		let input = document.createElement('input')
+		input.className = 'description'
+		input.type = 'text'
+		input.value = value
+		return input
+	}
+
+	getWrapper(wrapper) {
+		this.task.append(this.remove, this.checkBox, this.checkBoxIcon, this.description)
+		wrapper.className = 'task'
+		wrapper.setAttribute('data-task-id', this.id)
+		wrapper.append(this.task)
+		return wrapper
+	}
+
+	onRemove(event) {
+		if (event.target != this.remove) {
+			event.preventDefault()
+			return false
+		}
+		deleteTask(this.id).
+			then(ok => {
+				if (!ok) {
+					event.preventDefault()
+					return false
+				}
+			})
+		this.wrapper.dispatchEvent(new CustomEvent('escape-task', {
+			bubbles: true,
+			detail: {
+				remove: true,
+			}
+		}))
+	}
+
+	onCheckBox(event) {
+		if (event.target != this.checkBox) {
+			event.preventDefault()
+			return false
+		} else if (!this.description.value.trim()) { // if its input is empty
+			this.description.focus()
+			return false; // prevent from adding the task to the .completed-task-list
+		}
+
+		toggleCompletion(this.id).
+			then(ok => {
+				if (!ok) {
+					event.preventDefault()
+					return false
+				}
+			})
+
+		this.wrapper.dispatchEvent(new CustomEvent('escape-task', {
+			bubbles: true,
+			detail: {
+				remove: false,
+			}
+		}))
+		if (this.description.style.opacity = '0.4') {
+
+			this.description.style.opacity = '1'
+		} else {
+			this.description.style.opacity = '0.4'
+		}
+	}
+}
+
+
+class UserTask extends Task {
+	getDescriptionElement(value) {
+		let input = document.createElement('input')
+		input.className = 'description'
+		input.type = 'text'
+		input.value = value
+		input.readOnly = true
+		return input
+	}
+
+	getCheckBoxElement() {
+		let input = document.createElement('input')
+		input.type = 'checkbox'
+		input.onclick = (event) => {
+			event.preventDefault()
+		}
+		return input
+	}
+
+	getWrapper(wrapper) {
+		this.task.append(this.checkBox, this.checkBoxIcon, this.description)
+		wrapper.className = 'task'
+		wrapper.setAttribute('data-task-id', this.id)
+		wrapper.append(this.task)
+		return wrapper
+	}
+
+	onCheckBox() {
+		this.wrapper.dispatchEvent(new CustomEvent('escape-task', {
+			bubbles: true,
+			detail: {
+				remove: false,
+			}
+		}))
+
+		this.checkBox.checked = this.checkBox.checked ? false : true
+
+		if (this.description.style.opacity = '0.4') {
+			this.description.style.opacity = '1'
+		} else {
+			this.description.style.opacity = '0.4'
+		}
+	}
+
+}
+
+async function deleteTask(taskID) {
+	try {
+
+		let resp = await fetch('/tasks/' + taskID, {
+			method: 'DELETE',
+			headers: {
+				'Content-type': 'application/json',
+				'Accept': 'application/json',
+			}
+		})
+
+		if (resp.ok) {
+			return true
+		}
+		return false
+	} catch (err) {
+		console.error(err)
+		return false
+	}
+}
+
+async function toggleCompletion(taskID) {
+	try {
+
+		let resp = await fetch('/tasks/' + taskID, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			},
+			body: JSON.stringify({
+				toggleCompletion: true,
+			}),
+		})
+		if (resp.ok) {
+			return true
+		}
+		return false
+	} catch (err) {
+		console.error(err)
+		return false
+	}
+}
